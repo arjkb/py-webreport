bbd8ff107f06e370f46e8e2a5965b9a0b1cc0fb1
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-02-03 11:39:32 -0700

    create simple webpage

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ def main():
          ('Emily', 23, 'Dallas'),
         ]
     print(a)
-    print("hello")
+
+    with open('index.html', mode='w', encoding='utf-8') as f:
+        f.write("<html>")
+        f.write("<head>")
+        f.write("<title>My Report</title>")
+        f.write("</head>")
+        f.write("<body>")
+        f.write("<p> This is a sample text</p>")
+        f.write("</body>")
+
+    print("Report created as index.html")
+
 if __name__ == '__main__':
     main()
