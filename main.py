e1e2d6fc4c08355fc4013ba50851e9fb3c23e470
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-02-03 11:34:11 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def main():
+    a = [('Alice', 20, 'New York'),
+         ('Bob', 22, 'Philadelphia'),
+         ('Charlotte', 24, 'London'),
+         ('Dylan', 21, 'Los Angeles'),
+         ('Emily', 23, 'Dallas'),
+        ]
+    print(a)
+    print("hello")
+if __name__ == '__main__':
+    main()
